dd98d2e4f7799e64f6153611663fcef21370818f
eden <85859364+eden-jpg@users.noreply.github.com>
2022-07-27 20:57:41 -0500

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
-const fs =require('fs')
+const fs =require('fs');
+
 
 function getNotes(){
     let data =fs.readFileSync(path.join(__dirname,'../db/db.json'), 'utf-8')
@@ -15,12 +16,25 @@ function addNotes(newNote){
     let note=JSON.parse(data).note   
     const {title, text}=newNote
     const newNoteSaved ={title, text, id: Math.random().toString()}
+    console.log(newNoteSaved.id)
     note.push(newNoteSaved)
     fs.writeFileSync(
         path.join(__dirname,'../db/db.json'), 
         JSON.stringify({note}, null, 2)
     )
 
+}
+
+function deleteNotes(oldNoteId){
+    let data =fs.readFileSync(path.join(__dirname,'../db/db.json'), 'utf-8')
+    let note=JSON.parse(data).note  
+    const oldNote = note.filter(function(item){
+        return oldNoteId !== item.id
+    })
+    fs.writeFileSync(
+        path.join(__dirname,'../db/db.json'), 
+        JSON.stringify({note:oldNote}, null, 2)
+    )
 }
 
 router.get('/notes', (req, res)=>{
@@ -30,6 +44,10 @@ router.get('/notes', (req, res)=>{
 
 router.post('/notes', (req, res)=>{
     res.json(addNotes(req.body))
+})
+
+router.delete('/notes/:id', (req,res)=>{
+    res.json(deleteNotes(req.params.id))
 })
 
 module.exports = router
